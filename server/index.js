5d897a14d9b92fd20fe2df472d08618967cace82
Churchjg <churchjg283@gmail.com>
2021-02-11 18:58:04 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -17,8 +17,11 @@ app.use(cors()); //can now call cors with a function
 app.use('/posts', postRoutes); //every route inside of the postRoutes will start with posts (localhost:5000/posts)
 app.use("/user", userRouter);
 
+app.get('/', (req, res) => {
+    res.send('Hello to Memories')
+});
 
-
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true}) //function to accept connection and object parameters
     .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))) //find our port
