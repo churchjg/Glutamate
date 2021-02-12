92ece35522e976649b13f6180534e49e36c86ab7
Churchjg <churchjg283@gmail.com>
2021-02-11 19:11:17 -0500

    fixed

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
     title: String,
@@ -6,20 +6,16 @@ const postSchema = mongoose.Schema({
     creator: String,
     tags: [String],
     selectedFile: String,
-    likes: { type: [String], default: [] },
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
     createdAt: {
         type: Date,
         default: new Date(),
     },
 })
 
-const router = express.Router();
+var PostMessage = mongoose.model('PostMessage', postSchema);
 
-router.get('/', getPosts);
-router.post('/', createPost);
-router.get('/:id', getPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
-
-export default router;
+export default PostMessage;
