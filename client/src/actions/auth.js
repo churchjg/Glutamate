d24a14db23c45ebbf451cd7cf4af4385eea38986
Churchjg <churchjg283@gmail.com>
2021-02-24 15:35:03 -0500

    sign in alert

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,13 +3,13 @@ import * as api from '../api/index.js';
 
 
 export const signin = (formData, history) => async (dispatch) => {
+  alert("Signing you in!")
   try {
     const { data } = await api.signIn(formData);
 
     dispatch({ type: AUTH, data });
 
     history.push('/');
-    alert("Signing you in!")
   } catch (error) {
     alert(error);
   }
@@ -18,13 +18,14 @@ export const signin = (formData, history) => async (dispatch) => {
 
 
 export const signup = (formData, history) => async (dispatch) => {
+  alert("Creating New Account")
   try {
     const { data } = await api.signUp(formData);
 
     dispatch({ type: AUTH, data });
 
     history.push('/');
-    alert("Processing")
+    
   } catch (error) {
     alert(error);
   }
